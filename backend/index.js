6fba51eccf29723367dd6dae1deeec007a57bc15
Ahlyab <ahlyabasad@gmail.com>
2025-03-15 16:22:40 +0500

    Cors configured

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,40 @@
-const  dotenv = require('dotenv').config();
-const express = require('express');
+const dotenv = require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
-
 const port = process.env.PORT || 3000;
-const connectDB = require('./config/db');
+const connectDB = require("./config/db");
 // connectDB();
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/expense', require('./routes/expenseRoutes'));
-app.use('/api/user', require('./routes/userRoutes'));
+app.use("/api/expense", require("./routes/expenseRoutes"));
+app.use("/api/user", require("./routes/userRoutes"));
 
-app.listen(port, async () => {
-  console.log(`Server is running on port ${port}`);
+// app.listen(port, async () => {
+//   console.log(`Server is running on port ${port}`);
 
-  try {
-    await connectDB()
-    console.log('Database connected');
-  }
-  catch (error) {
-    console.error('Failed to connect to database');
-    process.exit(1);
-  }
+//   try {
+//     await connectDB();
+//     console.log("Database connected");
+//   } catch (error) {
+//     console.error("Failed to connect to database");
+//     process.exit(1);
+//   }
+// });
 
+connectDB().then(() => {
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
 });
